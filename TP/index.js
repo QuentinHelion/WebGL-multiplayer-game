2ce5ecb498a6aa87a1e5a49f746138c3a7130921
Quentin Hélion <helion.quentin@gmail.com>
2022-01-20 15:17:11 +0100

    multiplayers: work in progress

--- TP/index.js
+++ TP/index.js
@@ -14,6 +14,8 @@ console.log('starting...');
 io.sockets.on('connection', function(socket){
   console.log('connection');
 	socket.userData = { x:0, y:0, z:0, heading:0 };//Default values;
+  console.log(socket.userData);
+  socket.emit('userData', {x: socket.userData.x, y: socket.userData.y, z: socket.userData.z });
 
 	console.log(`${socket.id} connected`);
 	socket.emit('setId', { id:socket.id });
@@ -24,15 +26,15 @@ io.sockets.on('connection', function(socket){
     });
 
 	socket.on('init', function(data){
-		console.log(`socket.init ${data.model}`);
-		socket.userData.model = data.model;
-		socket.userData.colour = data.colour;
+		console.log(`socket.init ${data.x}`);
+		// socket.userData.model = data.model;
+		// socket.userData.colour = data.colour;
 		socket.userData.x = data.x;
 		socket.userData.y = data.y;
 		socket.userData.z = data.z;
 		socket.userData.heading = data.h;
-		socket.userData.pb = data.pb,
-		socket.userData.action = "Idle";
+		// socket.userData.pb = data.pb,
+		// socket.userData.action = "Idle";
 	});
 
 	socket.on('update', function(data){
@@ -40,18 +42,19 @@ io.sockets.on('connection', function(socket){
 		socket.userData.y = data.y;
 		socket.userData.z = data.z;
 		socket.userData.heading = data.h;
-		socket.userData.pb = data.pb,
-		socket.userData.action = data.action;
+    console.log("x: "+data.x);
+		// socket.userData.pb = data.pb,
+		// socket.userData.action = data.action;
 	});
 
-	socket.on('chat message', function(data){
-		console.log(`chat message:${data.id} ${data.message}`);
-		io.to(data.id).emit('chat message', { id: socket.id, message: data.message });
-	})
+	// socket.on('chat message', function(data){
+	// 	console.log(`chat message:${data.id} ${data.message}`);
+	// 	io.to(data.id).emit('chat message', { id: socket.id, message: data.message });
+	// })
 });
 
-http.listen(2002, function(){
-  console.log('listening on *:2002 - origin set');
+http.listen(3000, function(){
+  console.log('listening on -p 3000 - origin set');
 });
 
 setInterval(function(){
@@ -60,20 +63,20 @@ setInterval(function(){
 
     for(let id in io.sockets.sockets){
         const socket = nsp.connected[id];
-		//Only push sockets that have been initialised
-		if (socket.userData.model!==undefined){
-			pack.push({
-				id: socket.id,
-				model: socket.userData.model,
-				colour: socket.userData.colour,
-				x: socket.userData.x,
-				y: socket.userData.y,
-				z: socket.userData.z,
-				heading: socket.userData.heading,
-				pb: socket.userData.pb,
-				action: socket.userData.action
-			});
-		}
+  		//Only push sockets that have been initialised
+  		if (socket.userData.model!==undefined){
+  			pack.push({
+  				id: socket.id,
+  				model: socket.userData.model,
+  				colour: socket.userData.colour,
+  				x: socket.userData.x,
+  				y: socket.userData.y,
+  				z: socket.userData.z,
+  				heading: socket.userData.heading,
+  				pb: socket.userData.pb,
+  				action: socket.userData.action
+  			});
+  		}
     }
 	if (pack.length>0) io.emit('remoteData', pack);
 }, 40);
